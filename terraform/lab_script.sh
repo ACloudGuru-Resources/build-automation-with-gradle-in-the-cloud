8f8c33019324af513918e5b1883a99a90d5ecbee
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2023-08-21 19:34:02 -0400

    Update lab_script.sh

--- terraform/lab_script.sh
+++ terraform/lab_script.sh
@@ -4,8 +4,3 @@ curl -s "https://get.sdkman.io" | bash
 source "/home/cloud_user/.sdkman/bin/sdkman-init.sh"
 sdk install java
 sdk install gradle
-mkdir -p gradle_tf_lab
-git clone https://github.com/pluralsight-cloud/build-automation-with-gradle-in-the-cloud.git /home/cloud_user/gradle_tf_lab
-mv gradle_tf_lab/terraform/* gradle_tf_lab/
-rm -rf gradle_tf_lab/AWS
-rm -rf gradle_tf_lab/terraform
